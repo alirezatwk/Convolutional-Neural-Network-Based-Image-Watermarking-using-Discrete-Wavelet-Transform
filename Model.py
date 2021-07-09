4c2f7050603c8f2cb2868adf8919fdbf6d4259c8
Alireza Tavakoli <alirezata3akoli@gmail.com>
2021-07-09 22:56:56 +0430

    Extract methods and complete network

--- Model.py
+++ Model.py
@@ -1,17 +1,18 @@
-from IPython.core.display import Image
+# from IPython.core.display import Image
 import numpy as np
 import tensorflow as tf
 from tensorflow.python import keras
 from tensorflow.python.keras import layers
 from tensorflow.python.keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, Lambda, Reshape
 from tensorflow.python.keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D, Concatenate
+from tensorflow.python.keras.layers import UpSampling2D
 from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.preprocessing import image
-from tensorflow.python.keras.utils import layer_utils
-from tensorflow.python.keras.utils.data_utils import get_file
-from tensorflow.python.keras.applications.imagenet_utils import preprocess_input
+# from tensorflow.python.keras.utils import layer_utils
+# from tensorflow.python.keras.utils.data_utils import get_file
+# from tensorflow.python.keras.applications.imagenet_utils import preprocess_input
 # import pydot
-from IPython.display import SVG
+# from IPython.display import SVG
 #from keras.utils.vis_utils import model_to_dot
 #from keras.utils import plot_model
 
@@ -20,35 +21,112 @@ from IPython.display import SVG
 # import keras.backend as K
 # K.set_image_data_format('channels_last')
 
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import imshow
+# import matplotlib.pyplot as plt
+# from matplotlib.pyplot import imshow
+# from tensorflow.python.ops.gen_array_ops import size
 
 
-def FinalModel(images, watermarks):
+def FinalModel(imagesShape, watermarksShape):
 
-    # Images
-    x = Lambda(lambda c: tf.image.rgb_to_grayscale(c))(images) 
-    x = Conv2D(64, (3, 3), padding="same")(x)
+    images = Input(imagesShape)
+    watermarks = Input(watermarksShape)
 
-    # Watermarks
-    # 4 layer : 1,2,3) CONV => RELU => POOL 4) CL => POOL
-    y = Reshape((32, 32, 1), input_shape=(1024,))(watermarks)
-    # print(y)
-
-    #  first layer
-    y = Conv2D(512, (3, 3), strides=(1, 1))(y)
-    y = BatchNormalization(axis=2)(y)
-    y = Activation('relu')(y)
-    y = AveragePooling2D(pool_size=(2, 2))
-    
-    model = Model(inputs=watermarks, outputs=y)
-    model.summary()
-
+    x = hostImageNetwork(images)
+    y = WMNetwork(watermarks)
 
     # Concatenate
-#    con = Concatenate([x, y], axis = 2)
+    con = Concatenate(axis=-1)([x, y])
 
-FinalModel(np.zeros((3, 512, 512, 3)), np.zeros((3, 32 * 32)))
+    WMImage = embeddingNetwork(con)
+    ext = extractionNetwork(WMImage)
+
+    model = Model(inputs=[images, watermarks], outputs=[WMImage, ext], name="embedingNetwork")
+    model.summary()
+
+def hostImageNetwork(images):
+    x = Lambda(lambda c: tf.image.rgb_to_grayscale(c))(images) 
+    x = Conv2D(64, (3, 3), padding="same")(x)
+    return x
+
+def WMNetwork(watermarks):
+    # 4 layers : 1,2,3) CONV => RELU => POOL 4) CL => POOL
+    y = Reshape((32, 32, 1), input_shape=(1024,))(watermarks)
+    
+    # First layer
+    y = UpSampling2D(size=(2, 2))(y) # Use for covering stride = 0.5 of Conv2D
+    y = Conv2D(512, (3, 3), padding="same", strides=(1, 1))(y)
+    y = BatchNormalization()(y)
+    y = Activation('relu')(y)
+    y = AveragePooling2D(pool_size=(2, 2), strides=(1, 1), padding="same")(y)
+
+    # Second layer
+    y = UpSampling2D(size=(2, 2))(y) # Use for covering stride = 0.5 of Conv2D
+    y = Conv2D(128, (3, 3), padding="same", strides=(1, 1))(y) # 128 according to Figure 2 TODO: ask Zahra what to do with this number
+    y = BatchNormalization()(y)
+    y = Activation('relu')(y)
+    y = AveragePooling2D(pool_size=(2, 2), strides=(1, 1), padding="same")(y)
+    
+    # Third layer
+    y = UpSampling2D(size=(2, 2))(y) # Use for covering stride = 0.5 of Conv2D
+    y = Conv2D(64, (3, 3), padding="same", strides=(1, 1))(y)
+    y = BatchNormalization()(y)
+    y = Activation('relu')(y)
+    y = AveragePooling2D(pool_size=(2, 2), strides=(1, 1), padding="same")(y)
+
+    # Fourth layer
+    y = UpSampling2D(size=(2, 2))(y) # Use for covering stride = 0.5 of Conv2D
+    y = Conv2D(1, (3, 3), padding="same", strides=(1, 1))(y)
+    y = AveragePooling2D(pool_size=(2, 2), strides=(1, 1), padding="same")(y)
+    return y
+
+def embeddingNetwork(con):
+    # 4 layers : 1,2,3) CONV => BN => RELU 4) CL => TANH
+
+    # First layer
+    con = Conv2D(64, (3, 3))(con)
+    con = BatchNormalization()(con)
+    con = Activation('relu')(con)
+
+    # Second layer
+    con = Conv2D(64, (3, 3))(con)
+    con = BatchNormalization()(con)
+    con = Activation('relu')(con)
+
+    # Third layer
+    con = Conv2D(64, (3, 3))(con)
+    con = BatchNormalization()(con)
+    con = Activation('relu')(con)
+    
+    # Fourth layer
+    con = Conv2D(1, (3, 3))(con)
+    WMImage = Activation('tanh', name="embeddedImage")(con)
+    return WMImage
+
+def extractionNetwork(WMImage):
+    # 4 layers : 1,2,3) CONV => BN => ReLU 4) CL => tanh
+
+    # First layer
+    ext = Conv2D(128, (3, 3), strides=(2, 2), padding="same")(WMImage)
+    ext = BatchNormalization()(ext)
+    ext = Activation('relu')(ext)
+
+    # Second layer
+    ext = Conv2D(256, (3, 3), strides=(2, 2), padding="same")(ext)
+    ext = BatchNormalization()(ext)
+    ext = Activation('relu')(ext)
+
+    # Third layer
+    ext = Conv2D(512, (3, 3), strides=(2, 2), padding="same")(ext)
+    ext = BatchNormalization()(ext)
+    ext = Activation('relu')(ext)
+    
+    # Fourth layer
+    ext = Conv2D(1, (3, 3), strides=(2, 2), padding="same")(ext)
+    ext = Activation('tanh', name="outputWatermark")(ext)
+    return ext
+
+
+FinalModel((512, 512, 3), (32 * 32,))
 
 
 
